25b34a7e9a3233059d0c57f29b9e7973eb79ba13
pfpfdev <pfpfdev@gmail.com>
2022-07-03 00:17:04 +0900

    entryアドオンを作成

--- addons/entry/entry.js
+++ addons/entry/entry.js
@@ -1,9 +1,131 @@
 console.log("entry loaded")
 
 const Entry = {
-    check:(data)=>{
-        return "aaaaa"
+    isValid: (config, data) => {
+        return Entry._isValid(config, data).length === 0
+    },
+    abstruct: (config, data) => {
+        const err = Entry._isValid(config, data)
+        if (err !== "") {
+            return "解析エラー：" + err
+        }
+        const rows = data.entry.filter(e => "選手名(姓)" in e)
+        return `${rows.length}件のデータを読み込み`
+    },
+    parse: (data) => {
+        const errors = []
+        const rules = {}
+        const entries = []
+        if (!("entry" in data)) {
+            return {
+                errors: ["「entry」シートがありません"]
+            }
+        }
+        const rows = data.entry.filter(e => "選手名(姓)" in e)
+        for (const e of rows) {
+            const no = e["No."]
+            const rule = e["種目"]
+            // parse the rule
+            if (no in rules) {
+                if (rule !== rules[no]) {
+                    errors.push(`Noと種目に矛盾があります`)
+                    continue
+                }
+            } else {
+                rules[no] = rule
+            }
+            // parse player
+            if (!["正", "OP"].includes(e["区分"])) {
+                errors.push(`未定義の区分が入力されています「${e["区分"]}」@No.${no}`)
+                continue
+            }
+            entries.push({
+                family: e["選手名(姓)"],
+                last: e["選手名(名)"],
+                family_kana: e["フリガナ(セイ)"],
+                last_kana: e["フリガナ(メイ)"],
+                class: e["区分"],
+                grade: e["学年"],
+                time: parseInt(e["分"]) * 60 + parseInt(e["秒"]) + parseInt(e["00"]) / 100
+            })
+        }
+        return {
+            errors, rules, entries
+        }
+    },
+    ui: (RUNTIME) => {
+        const body = document.getElementById("body")
+        body.insertAdjacentHTML("beforeend", document.getElementById("entry").innerHTML)
+        const groups = {}
+        RUNTIME.Files.forEach(f => {
+            const group = f.config["団体名"]
+            if (!(group in groups)) {
+                groups[group] = f.data.entries
+            } else {
+                groups[group].push(...f.data.entries)
+            }
+        })
+        console.log(groups)
+        let template = document.getElementById("entry::group_stat_tmp")
+        for (const g in groups) {
+            let html = template.innerHTML
+            html = html.replace("$GROUP", g)
+            html = html.replace("$GROUP_STAT", groups[g].length)
+            document.getElementById("entry::group_stat").insertAdjacentHTML("beforeend", html)
+        }
+        RUNTIME.Files.forEach(f => {
+            template = document.getElementById("entry::warn_tmp")
+            if (f.data.errors.length > 0) {
+                for (const e of f.data.errors) {
+                    let html = template.innerHTML
+                    html = html.replace("$MSG", `${e} @ ${f.name}`)
+                    document.getElementById("entry::warnings").insertAdjacentHTML("beforeend", html)
+                }
+            } else {
+                let html = template.innerHTML
+                html = html.replace("$MSG", `正常 @ ${f.name}`)
+                document.getElementById("entry::warnings").insertAdjacentHTML("beforeend", html)
+            }
+        })
+    },
+    _isValid: (config, data) => {
+        if (!("団体名" in config)) {
+            return "団体名が空欄です"
+        }
+        if (!("記入責任者" in config)) {
+            return "記入責任者が空欄です"
+        }
+        if (!("責任者メールアドレス" in config)) {
+            return "責任者メールアドレスが空欄です"
+        }
+        if (!("entry" in data)) {
+            return "「entry」シートが見つかりません"
+        }
+        const rows = data.entry.filter(e => "選手名(姓)" in e)
+        for (e of rows) {
+            const required = ["No.", "種目", "区分", "選手名(姓)", "フリガナ(セイ)", "学年", "分", "秒", "00"]
+            for (r of required) {
+                if (!(r in e)) {
+                    return `「${r}」が不足している行があります`
+                }
+            }
+            const mustNumber = ["No.", "分", "秒", "00"]
+            for (r of mustNumber) {
+                if (!isFinite(parseInt(e[r]))) {
+                    return `「${r}」が異常値です：${e[r]}`
+                }
+            }
+        }
+        return ""
     }
 }
 
-Parser["entry"] = Entry
+RegisterParser("entry", Entry)
+
+const loadHTML = async () => {
+    var el = document.createElement("template")
+    const html = await (await fetch("./addons/entry/index.html")).text()
+    document.body.insertAdjacentHTML("beforeend", html)
+}
+
+loadHTML()
